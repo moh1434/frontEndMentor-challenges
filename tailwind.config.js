62bc735704bb284827911712abdd2f599bacb7ac
moh1434 <moh1434.ma@gmail.com>
2021-09-24 23:20:28 +0300

    set the fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
+			fontFamily: {
+				'Rubik': ['Rubik', 'Arial', 'sans-serif']
+			},
+			colors: {
+				"blue-grayish": "hsl(206, 6%, 79%)",
+				violet: {
+					// Primary
+					"pale": "hsl(276, 100%, 81%)",
+					"moderate": "hsl(276, 55%, 52%)",
+					"dark": "hsl(271, 15%, 43%)",
+					"very-dark": "hsl(271, 36%, 24%)",
+					"dark-grayish": "hsl(270, 7%, 64%)",
+					// Secondary
+					"light-grayish": "hsl(270, 20%, 96%)",
+				},
+				magenta: {
+					"very-light ": "hsl(289, 100%, 72%)"
+				}
+			}
 		},
 	},
 	variants: {
